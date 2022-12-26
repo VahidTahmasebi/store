63f5cb2b153ff0bc69be35c10940266a6e667647
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-26 18:42:07 +0330

    Add products data

---
+++ products.js
@@ -0,0 +1,44 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "bed",
+    price: 100.999,
+    imageUrl: "./image/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "bed",
+    price: 105.999,
+    imageUrl: "./image/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: "bed",
+    price: 106.999,
+    imageUrl: "./image/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "bed",
+    price: 103.999,
+    imageUrl: "./image/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "island chair",
+    price: 34.999,
+    imageUrl: "./image/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "sofa",
+    price: 70.999,
+    imageUrl: "./image/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "bed sofa",
+    price: 101.999,
+    imageUrl: "./image/product-7.jpeg",
+  },
+];
