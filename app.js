57611012df46bb7337b59ad6cc07ecb87dd1208b
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-26 18:43:53 +0330

    Product data access to the program

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+import { productsData } from "./products.js";
+
 const cartBtn = document.querySelector(".cartBtn");
 const cartModal = document.querySelector(".cartModal");
 const backDrop = document.querySelector(".backDrop");
 const confirmModalBtn = document.querySelector(".confirmModalBtn");
+
+class Products {
+  getProduct() {
+    return productsData;
+  }
+}
+
+class UI {}
+
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const product = new Products();
+  const productsData = product.getProduct();
+  console.log(productsData);
+});
 
 function showModalFunction() {
   backDrop.style.display = "block";
